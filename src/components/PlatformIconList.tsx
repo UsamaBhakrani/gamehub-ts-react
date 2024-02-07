16257192ee46d0ae8a15e6e0c8ddee04322b866d
UsamaBhakrani <usamabhakrani@gmail.com>
2024-02-08 00:02:45 +0500

    iconmap

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,41 @@
+import { HStack, Icon } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaApple,
+  FaXbox,
+  FaAndroid,
+  FaLinux,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    mac: FaApple,
+    xbox: FaXbox,
+    android: FaAndroid,
+    linux: FaLinux,
+    iphone: MdPhoneIphone,
+    nintendo: SiNintendo,
+    web: BsGlobe,
+  };
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
