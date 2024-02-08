353d34ee9d0f1009f132871ed69a1fbadded3676
UsamaBhakrani <usamabhakrani@gmail.com>
2024-02-08 17:52:27 +0500

    Display Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,28 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
 
   return (
-    <ul>
+    <List>
       {data.map((g) => {
-        return <li key={g.id}>{g.name}</li>;
+        return (
+          <ListItem key={g.id} paddingY="5px">
+            <HStack>
+              <Image
+                margin={1}
+                boxSize="30px"
+                borderRadius={8}
+                src={getCroppedImageUrl(g.image_background)}
+              />
+              <Text fontSize='large'>{g.name}</Text>
+            </HStack>
+          </ListItem>
+        );
       })}
-    </ul>
+    </List>
   );
 };
 
